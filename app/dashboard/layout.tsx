5dc662d6788115c8cc5be0e142ea3de05af02ecd
coding404life <coding404life@gmail.com>
2025-01-22 02:42:25 +0200

    feat: add dashboard components

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface Props {
+    children: React.ReactNode
+}
+
+export default function Layout({ children }: Readonly<Props>) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
