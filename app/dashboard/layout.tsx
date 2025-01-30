5cd7ce629361bcc5df668b1489337037914ab032
Yahya <kingwar66@gmail.com>
2025-01-31 01:16:06 +0200

    chore: update skeleton with suspense and add PPR(Partial prerendering) for dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { Metadata } from 'next';
 
+export const experimental_ppr = true;
+
 export const metadata: Metadata = {
     title: 'Dashboard',
     description: 'Dashboard',
@@ -16,7 +18,10 @@ export default function Layout({ children }: Readonly<Props>) {
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+
+            <main className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </main>
         </div>
     );
 }
