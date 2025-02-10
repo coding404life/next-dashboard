8d0f850edee1499465d2dc798cc5850c2e0bf965
Yahya <kingwar66@gmail.com>
2025-02-10 09:27:34 +0200

    chore: wrap the customer table around suspense

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,12 @@
 import { fetchCustomers } from "@/app/lib/data"
 import CustomersTable from "@/app/ui/customers/table"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+import { Suspense } from "react"
 
 export default async function Customers() {
     const customers = await fetchCustomers()
 
-    return <CustomersTable customers={customers} />
+    return <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+    </Suspense>
 }
