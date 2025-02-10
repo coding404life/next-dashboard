6b4d325d11d10eb6562aa67b3942f6a662d7fabf
Yahya <kingwar66@gmail.com>
2025-02-10 09:20:16 +0200

    feat: adding customer table + adjusting the sql query for fetching customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Customers() {
-    return <p>customers</p>
+import { fetchCustomers } from "@/app/lib/data"
+import CustomersTable from "@/app/ui/customers/table"
+
+export default async function Customers() {
+    const customers = await fetchCustomers()
+    console.log(customers)
+    return <CustomersTable customers={customers} />
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -6,6 +6,7 @@ import {
   InvoicesTable,
   LatestInvoiceRaw,
   Revenue,
+  FormattedCustomersTable,
 } from "./definitions";
 import { formatCurrency } from "./utils";
 
@@ -169,19 +170,24 @@ export async function fetchInvoiceById(id: string) {
 
 export async function fetchCustomers() {
   try {
-    const data = await sql<CustomerField>`
+    const data = await sql<FormattedCustomersTable>`
       SELECT
-        id,
-        name
+        customers.id,
+        customers.name,
+        customers.email,
+        customers.image_url,
+        COUNT(invoices.id) AS total_invoices,
+        SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+        SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
       FROM customers
-      ORDER BY name ASC
+      LEFT JOIN invoices ON customers.id = invoices.customer_id
+      GROUP BY customers.id, customers.name, customers.email, customers.image_url
+      ORDER BY customers.name ASC
     `;
-
-    const customers = data.rows;
-    return customers;
+    return data.rows;
   } catch (err) {
     console.error("Database Error:", err);
-    throw new Error("Failed to fetch all customers.");
+    throw new Error("Failed to fetch customer data.");
   }
 }
 
