1abd022491bf720ef0111b209d54904ef70587dd
Yahya <32603951+coding404life@users.noreply.github.com>
2025-02-10 10:48:07 +0200

    feat: adding customer table + adjusting the sql query for fetching cu… (#10)
    * feat: adding customer table + adjusting the sql query for fetching customers
    
    * fix: build issue due to missing vars
    
    * chore: wrap the customer table around suspense

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Customers() {
-    return <p>customers</p>
+import { fetchCustomers } from "@/app/lib/data"
+import CustomersTable from "@/app/ui/customers/table"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+import { Suspense } from "react"
+
+export default async function Customers() {
+    const customers = await fetchCustomers()
+
+    return <Suspense fallback={<InvoicesTableSkeleton />}>
+        <CustomersTable customers={customers} />
+    </Suspense>
 }
